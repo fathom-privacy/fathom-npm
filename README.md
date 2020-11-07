e175f910b89ae2b6bcfd1f6b3736c6bc049cae92
ZaneWithSpoon <zane.witherspoon@gmail.com>
2020-11-06 21:40:11 -0500

    lookup object is more useful and exportable

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ fathom-npm
 ![npm](https://img.shields.io/npm/v/fathom-privacy) 
 # Fathom Privacy NPM SDK
 
+This modules serves to simplify integrations with the GDPR enforcement APIs provided by https://fathomprivacy.com
+
 ## Usage instructions
 
 1. Install the fathom-privacy package 
@@ -17,43 +19,25 @@ npm i fathom-privacy --save
 <div id="fathom-signup" />
 ```
 
-3. Call the fathom function with your application_id and an OnAuthentication callback to initialize the button 
+3. Call the fathom function with your application_id 
 
 ~~~javascript
 import fathom from "fathom-privacy"
 
-fathom("your-application-id", (session_id, newLookup) => {
-    console.log("On authentication callback function")
-    console.log(session_id)
+// Fathom initializes button with id "fathom-signup"
+fathom("your-application-id").then((newLookup) => {     
+    //when the button is clicked, promise is resolved and returns a newLookup object
+    
+    newLookup.listenForStatus((message) => {
+        //listenForStatus(callback(message)) will run on every new status update
+        if (message.status === "in progress") {
+            console.log("sign up successful!")
+        }
 
-    //Check periodically for completion fo the data collection
-    setTimeout(() => {checkStatusPeriodically()}, 5000)
-
-    function checkStatusPeriodically() {
-        newLookup.getStatus()
-            .then((status) => {
-                if (status === "complete") {
-                    // when status is complete, fetch call the getLIContacts() functon on newLookup
-                    getContactsWhenComplete()
-                } else {
-                    console.log(status)
-                    setTimeout(() => {checkStatusPeriodically()}, 5000)
-                }
-            })
-            .catch((error) =>  {
-                console.log(error)
-            });
-    }
-
-    //When the collection is complete, call the newLookup's getLIComplete function
-    function getContactsWhenComplete() {
-        newLookup.getLIContacts()
-            .then((results) => {
-                console.log(results)
-            })
-            .catch((error) =>  {
-                console.log(error)
-            });
-    }
+        if (message.status === "complete") {
+            //newLookup's getLIData() function will return the data for that user's lookup 
+            newLookup.getLIData().then((results) => { console.log(results) })
+        }
+    })
 })
 ~~~ 
--- index.js
+++ index.js
@@ -13,53 +13,59 @@ if (dev) {
 } 
 
 //uses both callback and promise for backwards compatibility
-function fathomInit(application_id, callback)  {
+function fathomInit(application_id)  {
     //set style of button
     var x = document.getElementById("fathom-signup");
     x.style.cursor = "pointer";
     x.innerText = "Connect your LinkedIn with Fathom"
 
     //OnClick of that button, create a popup and on successful Sign in, both return callback (for backwards compatibility) and return a Lookup class Object
-    document.getElementById('fathom-signup').onclick = function openAuth() {
-        let session_id = uuidv4();
-        let popup = window.open(authServer+"?session_id="+session_id+"&application_id="+application_id,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+    return new Promise (function(resolve, reject) {
+        document.getElementById('fathom-signup').onclick = function openAuth() {
+            let session_id = uuidv4();
+            let popup = window.open(authServer+"?session_id="+session_id+"&application_id="+application_id,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 
-        // wait with websockets instead
-        let socketPath = wsServer + session_id;    
-        const chatSocket = new WebSocket(socketPath);
-
-        chatSocket.onmessage = (e) => {
-            var data = JSON.parse(e.data);
-            if (dev) {
-                console.log("onMessage is received!")
-                console.log(data)
-            }
-            if (data.status === "in progress" || data.status === "timed out awaining pin") {
-                //close window
-
-                //slight delay before closing to finish animation
-                setTimeout(() => {  popup.close() }, 1000);
-                callback(new Lookup(application_id, session_id), null);
-            }
+            let newLookup = new Lookup(application_id, session_id)
+            newLookup.listenForStatus((message) => {
+                if (message.status === "in progress" || message.status === "timed out awaining pin" || message.status === "unknown error") {
+                    //close window
+                    //slight delay before closing to finish animation
+                    setTimeout(() => {  popup.close() }, 1000);
+                }
+            })
+            resolve(newLookup)
         }
-
-    }
+    })
 }
 
+function lookup(application_id, session_id) {
+    return new Lookup(application_id, session_id)
+}
+
+//Lookup Class contains many helpful functions for interacting with the API
 class Lookup{
     constructor(application_id, session_id) {
         this.application_id = application_id;
         this.session_id = session_id;
     }
 
-    test() {
-        return "test!"
-    }
-
     getSessionId() {
         return this.session_id
     }
 
+    //connects to the websocket to listen for status updates
+    listenForStatus(onMessageReceivedCallback) {
+        // wait with websockets instead
+        let socketPath = wsServer + this.session_id;    
+        const chatSocket = new WebSocket(socketPath);
+
+        chatSocket.onmessage = (e) => {
+            var data = JSON.parse(e.data);
+            onMessageReceivedCallback(data)
+        }
+    }
+
+    //makes a http call to /getStatus to get the current status
     getStatus() {
         let that = this
         return new Promise (function(resolve, reject) {
@@ -79,12 +85,12 @@ class Lookup{
         })
     }
 
-    getLIContacts() {
+    getLIData(scope) {
         let that = this
         return new Promise (function(resolve, reject) {
             axios({
                 method: 'get',
-                url: apiServer +'/getLIContacts?session_id='+that.session_id,
+                url: apiServer +'/getLIData?session_id='+that.session_id,
                 headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer '+ that.application_id
@@ -100,4 +106,4 @@ class Lookup{
 }
 
 // export feature declared earlier as default
-export { Lookup, fathomInit as default };
+export { lookup, fathomInit as default };
